4f720d6ef3c119adceddce3a47c5425b81d56436
jiteshmotati <jmotati@purdue.edu>
2019-05-26 15:17:58 -0700

    email ID and password can be generated now

--- src/eMail.java
+++ src/eMail.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 /**
  * This is an email application!
  *
@@ -13,8 +14,11 @@ public class eMail {
     private String lastName;
     private String password;
     private String department;
+    private String emailID;
     private int mailboxCapacity;
+    private int defaultPassLength = 10;
     private String alternateEmail;
+    private String emailSuffix = "yourcompany.com";
 
     //Constructor to get first and last name
     public eMail(String firstName, String lastName) {
@@ -24,22 +28,46 @@ public class eMail {
 
         //Call a method asking for the department, return the department
         this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        //Call a method to return a random password.
+        this.password = randomPassword(defaultPassLength);
+        System.out.println("Your password is: " + this.password);
+
+        //Make an email id
+        emailID = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + emailSuffix;
+        System.out.println("Your email is: " + emailID);
     }
 
     //Ask for department
     private String setDepartment() {
-        System.out.print("Enter the department: \n1. Sales\n 2. For Development\n 3. Accounting\n 0. None");
+        System.out.print("Enter the department:\n 1. Sales\n 2. Development\n 3. Accounting\n 0. None\n");
         Scanner scan = new Scanner(System.in);
         int deptChoice = scan.nextInt();
         if (deptChoice == 1) {
+            //this.department = "Sales";
             return "sales";
         } else if (deptChoice == 2) {
+            //this.department = "Development";
             return "dev";
         } else if (deptChoice == 3) {
+            //this.department = "Accounting";
             return "acct";
         } else {
-            return "";
+            //this.department = "None";
+            return "None";
         }
+    }
+
+    //Generate a rand password
+    private String randomPassword(int length) {
+        String setPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+        char[] password = new char[length];
+        for (int i = 0; i < length; i++) {
+            int rand = (int) (Math.random() * setPassword.length());
+            password[i] = setPassword.charAt(rand);
+        }
+        return new String(password);
     }
 
     //Set the mailbox capacity
