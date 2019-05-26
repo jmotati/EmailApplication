418e2deb0e8a52f4b21fd3baba039c93a337deba
jiteshmotati <jmotati@purdue.edu>
2019-05-26 12:48:10 -0700

    Outline and department method

--- src/eMail.java
+++ src/eMail.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This is an email application!
  *
@@ -7,4 +9,42 @@
  *
  */
 public class eMail {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to get first and last name
+    public eMail(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+
+        //Call a method asking for the department, return the department
+        this.department = setDepartment();
+    }
+
+    //Ask for department
+    private String setDepartment() {
+        System.out.print("Enter the department: \n1. Sales\n 2. For Development\n 3. Accounting\n 0. None");
+        Scanner scan = new Scanner(System.in);
+        int deptChoice = scan.nextInt();
+        if (deptChoice == 1) {
+            return "sales";
+        } else if (deptChoice == 2) {
+            return "dev";
+        } else if (deptChoice == 3) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
 }
--- src/eMailApp.java
+++ src/eMailApp.java
@@ -1,2 +1,5 @@
 public class eMailApp {
+    public static void main(String[] args) {
+        eMail em1 = new eMail("John", "Smith");
+    }
 }
